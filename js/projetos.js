bb7ac39250803c1f2f2c10339fe715d8be19c4e3
Ageildo Vitorino <107153811+Ageildo@users.noreply.github.com>
2024-05-27 23:18:52 -0300

    Add files via upload

---
+++ js/projetos.js
@@ -0,0 +1,10 @@
+function projetos(section) {
+  section.innerHTML = `<header>
+      <h2>Projetos</h2>
+    </header>
+    <div class="content">
+      <div class="cards">
+      </div>`;
+}
+
+export { projetos };
